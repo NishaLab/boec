c0882fe45b3e4d1af0fdd9a9a618d8050a15b80a
namtrinh <sabo_nt>
2021-06-01 19:55:12 +0700

    add detailOder

--- boec_admin/urls.py
+++ boec_admin/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('boec/admin/',views.index, name='common'),
-    path('boec/admin/oders/',views.oders, name='list_order')
+    path('boec/admin/oders/',views.oders, name='list_order'),
+    path('boec/admin/oders/detail/<int:order_id>',views.detailOrder, name='detail_order')
 ]
--- boec_admin/views.py
+++ boec_admin/views.py
@@ -8,4 +8,16 @@ def index(request):
 
 def oders(request):
     list_Order = Order.objects.all()
+    # list_Amount = []
+    # for oder in list_Order:    
+    #     list_OrderProduct = OrderedProduct.objects.get(oder = oder.id)
+    #     total = 0
+    #     for item in list_OrderProduct:
+    #         total += item.price
+    #     list_Amount.append(total)
     return render(request, "common/oders.html", {"oders":list_Order} )
+
+def detailOrder(request,order_id):
+    products = OrderedProduct.objects.get(order = order_id)
+    return render(request, 'common/detail_order.html')
+    
