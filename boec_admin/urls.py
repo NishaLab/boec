5a839911dfb56b3b27b19c10f9bdf967ac4aa322
Dat <datnguyen08011999@gmail.com>
2021-06-01 21:48:26 +0700

    Dat new

--- boec_admin/urls.py
+++ boec_admin/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('boec/admin/',views.index),
-    path('boec/admin/oders/',views.oders)
+    path('boec/admin/oders/',views.oders),
+    path('productlist/',views.productlist, name="productlist"),
+    path('add_product/',views.addProduct, name="addProduct"),
 ]
