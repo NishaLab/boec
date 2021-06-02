5f987cc33c75a40a81d8f8df0c148d30ae50efdf
Nguyễn Đức Hưng <hungnguyenw420@gmail.com>
2021-06-02 23:27:44 +0700

    add favourite product item

--- boec_core/views/views.py
+++ boec_core/views/views.py
@@ -104,8 +104,6 @@ class RoleRequiredView(View):
 
 
 def get_user_role_root_path(user):
-    if user.role == 1:
-        return "/boec/customer"
     if user.role == 2:
         return "/boec/admin"       
-    return None
+    return "/boec/customer"
