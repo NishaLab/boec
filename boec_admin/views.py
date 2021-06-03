65d93e630f09fc2643507012687770812d451f92
Dat <datnguyen08011999@gmail.com>
2021-06-03 17:06:25 +0700

    admin inventory update

--- boec_admin/views.py
+++ boec_admin/views.py
@@ -24,35 +24,74 @@ def detailOrder(request,order_id):
     oders = [1,2,3,4,5,6]
     return render(request, "common/oders.html", {"oders":oders} )
 def productlist(request):
+    productVars = ProductVariant.objects.all()
     products = Product.objects.all()
     categorys = Category.objects.all()
     sub_categorys = SubCategory.objects.all()
-    return render(request, "common/productlist.html",{'products':products,'categorys':categorys,'sub_categorys':sub_categorys})
+    vendors = Vendor.objects.all()
+    # 
+    return render(request, "common/productlist.html",{'products':products,'categorys':categorys,'sub_categorys':sub_categorys,'vendors':vendors,'productVars':productVars})
+
+def addCategory(request):
+    name= request.GET['category_name']
+    discription =request.GET['category_desc']
+    category= Category( name=name, desc=discription)
+    category.save()
+    return redirect('productlist')
+
+def addSubCategory(request):
+    name= request.GET['subCategory_name']
+    discription =request.GET['subCategory_desc']
+    id_ca = request.GET['category']
+    category = Category.objects.get(id=id_ca)
+    subCategory= SubCategory( name=name, desc=discription,parent=category)
+    subCategory.save()
+    return redirect('productlist')
+
+def addVendor(request):
+    name= request.GET['vendor_name']
+    address= request.GET['vendor_address']
+    phone= request.GET['vendor_phone']
+    desc= request.GET['vendor_desc']
+    vendor= Vendor( name=name,address = address,phone=phone,desc=desc)
+    vendor.save()
+    return redirect('productlist')
 
 def addProduct(request):
     name= request.GET['product_name']
-    price = request.GET['price']
-    quantity = request.GET['quantity']
     id_ca = request.GET['category']
     id_sub = request.GET['subcategory']
     category = Category.objects.get(id=id_ca)
     sub = SubCategory.objects.get(id=id_sub)
     discription =request.GET['disscription']
-    category= Category( name='dien tu', desc='dat')
-    category.save()
     Product.objects.create(name=name, desc = discription,category= category,sub_category=sub)
     data = Product.objects.all()
     return redirect('productlist')
 
-def deleteProduct(request,product_id):
+def addProductVariant(request):
+    id_product = request.GET['product']
+    product = Product.objects.get(id=id_product)
+    quantity = request.GET['productVar_quantity']
+    price = request.GET['productVar_price']
+    saleOff = request.GET['productVar_sale_off']
+    id_vendor = request.GET['vendor']
+    vendor = Vendor.objects.get(id=id_vendor)
+    is_selling = request.GET['selling']
+    is_feature = request.GET['feature']
+    ProductVariant.objects.create(product=product,quantity=quantity,price=price,vendor=vendor,is_feature=is_feature,is_selling=is_selling,sale_off=saleOff)
+    return redirect('productlist')
 
-    Product.objects.get(id=product_id).delete()
+
+def deleteProduct(request,product_id):
+    ProductVariant.objects.get(id=product_id).delete()
+    # Product.objects.get(id=product_id).delete()
     return redirect('productlist')
 
 def editProduct(request,product_id):
     if request.method == 'POST':
         id = request.POST['id']
-        product = Product.objects.get(id=id)
+        productVariant = ProductVariant.objects.get(id=product_id)
+        product = productVariant.product
         name = request.POST['name']
         desc = request.POST['description']
         id_category = request.POST['category']
@@ -68,7 +107,22 @@ def editProduct(request,product_id):
 
 
 
-    product = Product.objects.get(id=product_id)
+    productVariant = ProductVariant.objects.get(id=product_id)
     categorys = Category.objects.all()
     sub_categorys = SubCategory.objects.all()
-    return render(request, "common/productEdit.html",{'product':product,'categorys':categorys,'sub_categorys':sub_categorys})
+    return render(request, "common/productEdit.html",{'productVariant':productVariant,'categorys':categorys,'sub_categorys':sub_categorys})
+
+
+# Search
+def productSearch(request):
+    
+    productName = request.GET['search_name']
+    if productName == "":
+        productVars =ProductVariant.objects.all()
+    else:
+        productVars =ProductVariant.objects.filter(product__name__icontains=productName)
+        
+    
+    # ,{'productVariant':productVariant,'productName':productName}
+    
+    return render(request, "common/productlist.html",{'productVars':productVars})
