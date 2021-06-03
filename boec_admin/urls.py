5ffc58f2340206d5eb5a067b90cd60a0808a3ffb
namtrinh <sabo_nt>
2021-06-03 16:51:28 +0700

    change status order

--- boec_admin/urls.py
+++ boec_admin/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('boec/admin/',views.index, name='common'),
-    path('boec/admin/oders/',views.oders, name='list_order'),
-    path('boec/admin/oders/detail/<int:order_id>',views.detailOrder, name='detail_order'),
+    path('boec_admin/',views.index, name='common'),
+    path('boec_admin/oders/',views.oders, name='list_order'),
+    path('boec_admin/edit_status/<int:order_id>',views.changeStatusOrder, name='changeStatus'),
+    path('boec_admin/oders/detail/<int:order_id>',views.detailOrder, name='detail_order'),
     path('boec_admin/productlist/',views.productlist, name="productlist"),
     path('boec_admin/add_product/',views.addProduct, name="addProduct"),
     path('boec_admin/edit_product/',views.editProduct, name="editProduct"),
--- boec_admin/views.py
+++ boec_admin/views.py
@@ -8,22 +8,13 @@ def index(request):
 
 def oders(request):
     list_Order = Order.objects.all()
-    list_Amount = []
-    for item in list_Order:    
-        list_OrderProduct = OrderedProduct.objects.filter(order = item.id)
-        total = 0
-        for item in list_OrderProduct:
-            total += item.price
-        list_Amount.append(total)
-    return render(request, "common/oders.html", {"oders":list_Order,"amount":list_Amount} )
+    
+    return render(request, "common/oders.html", {"oders":list_Order} )
 
 def detailOrder(request,order_id):
     order = Order.objects.filter(id = order_id)
     products = OrderedProduct.objects.filter(order = order_id)
     return render(request, 'common/detail_order.html',{'products':products, 'order':order})
-    
-    oders = [1,2,3,4,5,6]
-    return render(request, "common/oders.html", {"oders":oders} )
 
 def productlist(request):
     products = Product.objects.all()
@@ -44,3 +35,11 @@ def addProduct(request):
 
 def editProduct(request):
     return render(request, "common/productEdit.html")
+
+def changeStatusOrder(request, order_id):
+    order = Order.objects.get(pk = order_id)
+    value = request.POST["choice"]
+    order.status = value
+    order.save()
+    listOrder = Order.objects.all()
+    return render(request, 'common/oders.html', {"oders":listOrder})
